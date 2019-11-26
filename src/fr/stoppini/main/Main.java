85099e23d1a980ded1c49b07590e925a3bfa2633
Alexandre.Stoppini <stoppini.alexandre@gmail.com>
2019-11-26 15:52:23 +0100

    Création de l'ensemble des classes du projet. Choix mode : obvious Game : Permet de regrouper toute les instructions avant d'envoyer dans le main GenerationCodeManul : obvious VerificationCodeAuto : obvious VerificationCodeFather : Classe mère de VerifAuto et VerifManuel (à voir)

--- src/fr/stoppini/main/Main.java
+++ src/fr/stoppini/main/Main.java
@@ -1,4 +1,10 @@
 package fr.stoppini.main;
 
+import fr.stoppini.core.VerifCodeManuel;
+
 public class Main {
+    public static void main(String[] args) {
+        VerifCodeManuel myst = new VerifCodeManuel();
+
+    }
 }
